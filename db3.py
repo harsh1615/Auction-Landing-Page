ba82dd2bdafe5da8f200c41925a5931d063c07df
kartikbhargav11 <kartikbhargav2019@gmail.com>
2020-11-03 11:42:12 +0530

    remove unused code

--- db3.py
+++
@@ -1,6 +0,0 @@
-from basic import db, Puppy
-
-#create a new column#
-my_puppy = Puppy('rufus',5)
-db.session.add(my_puppy)
-db.session.commit()
--- index.py
+++ index.py
@@ -31,18 +31,14 @@ def index():
 def creation():
 	return render_template('creation.html')
 
-@app.route('/product/<id>')
-def product(id):
-	return render_template('item.html')
-
 @app.route('/wishlist')
 def wishlist():
 	return render_template('watchlist.html')
 
+
 @app.route('/product/<id>')
 def product(id):
-    return render_template('item.html')
-
+	return render_template('item.html')
 
 if __name__ == '__main__':
     app.run()
