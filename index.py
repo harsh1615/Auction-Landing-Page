5bd9f95c7341fc27b5f8d35a725238b6d2022201
kartikbhargav11 <kartikbhargav2019@gmail.com>
2020-11-08 13:48:47 +0530

    login and signup pages

--- index.py
+++ index.py
@@ -1,16 +1,27 @@
 import os
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
+from werkzeug.security import generate_password_hash, check_password_hash
 
-basedir = os.path.abspath(os.path.dirname(__file__))
+
+# bcrypt = Bcrypt()
+# password = "supersecretpassword"
+# hashed_password = bcrypt.generate_password_hash(password=password)
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "mysecretkey"
+basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-
 db = SQLAlchemy(app)
-
+Migrate(app, db)
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
 class Items(db.Model):
 	__tablename__ = 'products'
 	id = db.Column(db.Integer, primary_key=True)
@@ -26,6 +37,15 @@ class Items(db.Model):
 @app.route('/')
 def index():
 	return render_template('index.html')
+
+
+@app.route('/login')
+def login():
+	return render_template('login.html')
+
+@app.route('/signup')
+def signup():
+	return render_template('signup.html')
 
 @app.route('/creation')
 def creation():
