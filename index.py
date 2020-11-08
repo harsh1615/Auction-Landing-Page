bc543b6a2462735ab27b10d5086b0bb15faf0e55
kartikbhargav11 <kartikbhargav2019@gmail.com>
2020-11-08 15:33:42 +0530

    comment code

--- index.py
+++ index.py
@@ -1,10 +1,10 @@
 import os
 from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
-from flask_migrate import Migrate
-from flask_login import LoginManager
-from flask_bcrypt import Bcrypt
-from werkzeug.security import generate_password_hash, check_password_hash
+# from flask_sqlalchemy import SQLAlchemy
+# from flask_migrate import Migrate
+# from flask_login import LoginManager
+# from flask_bcrypt import Bcrypt
+# from werkzeug.security import generate_password_hash, check_password_hash
 
 
 # bcrypt = Bcrypt()
@@ -12,27 +12,27 @@ from werkzeug.security import generate_password_hash, check_password_hash
 # hashed_password = bcrypt.generate_password_hash(password=password)
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = "mysecretkey"
-basedir = os.path.abspath(os.path.dirname(__file__))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# app.config["SECRET_KEY"] = "mysecretkey"
+# basedir = os.path.abspath(os.path.dirname(__file__))
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db = SQLAlchemy(app)
+# db = SQLAlchemy(app)
 # Migrate(app, db)
 # login_manager = LoginManager()
 # login_manager.init_app(app)
 # login_manager.login_view = 'login'
-class Items(db.Model):
-	__tablename__ = 'products'
-	id = db.Column(db.Integer, primary_key=True)
-	name = db.Column(db.Text)
-	image = db.Column(db.Text)
-	base_price = db.Column(db.Integer)
+# class Items(db.Model):
+# 	__tablename__ = 'products'
+# 	id = db.Column(db.Integer, primary_key=True)
+# 	name = db.Column(db.Text)
+# 	image = db.Column(db.Text)
+# 	base_price = db.Column(db.Integer)
 
-	def __init__(self, name, image, base_price):
-		self.name = name
-		self.image = image
-		self.base_price = base_price
+# 	def __init__(self, name, image, base_price):
+# 		self.name = name
+# 		self.image = image
+# 		self.base_price = base_price
 
 @app.route('/')
 def index():
